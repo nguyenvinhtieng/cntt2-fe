c8dd7ec33a72dd8a14a7d647ca4dff82c5532b9a
Vinh.Tieng <vinh.tieng@laichi.vn>
2023-02-17 14:02:06 +0700

    fix bug

--- layouts/DefaultLayout/DefaultLayout.jsx
+++ layouts/DefaultLayout/DefaultLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Overlay from "~/components/Overlay/Overlay";
 import Header from "~/layouts/components/Header/Header";
-import Sidebar from "../components/Sidebar/Sidebar";
+import Sidebar from "~/layout/components/Sidebar/Sidebar";
 export default function DefaultLayout({ children }) {
   const [isShowSidebar, setIsShowSidebar] = React.useState(false);
   const toggleSidebar = () => setIsShowSidebar(!isShowSidebar);
